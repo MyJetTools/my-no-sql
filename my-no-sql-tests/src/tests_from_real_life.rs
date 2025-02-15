70a78a9b41f6609f3bb784da6b58cf25e5187526
amigin <amigin@gmail.com>
2025-02-15 11:40:26 +0200

    Fixes

--- my-no-sql-tests/src/tests_from_real_life.rs
+++ my-no-sql-tests/src/tests_from_real_life.rs
@@ -12,7 +12,9 @@ pub struct PayoutWithdrawalSettingsMyNoSqlEntity {
 #[cfg(test)]
 mod tests {
 
-    use my_no_sql_sdk::abstractions::MyNoSqlEntitySerializer;
+    use my_no_sql_sdk::{
+        abstractions::MyNoSqlEntitySerializer, core::rust_extensions::date_time::DateTimeStruct,
+    };
 
     use super::PayoutWithdrawalSettingsMyNoSqlEntity;
 
@@ -25,5 +27,18 @@ mod tests {
 
         assert_eq!(entity.value, 15000.0);
         assert_eq!(entity.currency, "USD");
+        assert_eq!(entity.partition_key, "bank-transfer");
+        assert_eq!(entity.row_key, "max");
+
+        let dt_struct: DateTimeStruct = entity.time_stamp.to_date_time().into();
+
+        assert_eq!(dt_struct.year, 2024);
+        assert_eq!(dt_struct.month, 11);
+        assert_eq!(dt_struct.day, 29);
+
+        assert_eq!(dt_struct.time.hour, 14);
+        assert_eq!(dt_struct.time.min, 59);
+        assert_eq!(dt_struct.time.sec, 15);
+        assert_eq!(dt_struct.time.micros, 614500);
     }
 }
